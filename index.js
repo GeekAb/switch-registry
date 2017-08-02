c6b1194da828e01b1f481572ffabfcb141e4aa07
Abhishek <nit.abhi85@gmail.com>
2017-08-01 17:34:24 -0700

    Adding app's skeleton

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Module exports.
+ * @public
+ */
+
+module.exports = usage;
+module.exports.list = list;
+module.exports.add = add;
+module.exports.remove = remove;
+module.exports.change = change;
+
+function usage () {
+
+}
+
+function list () {
+
+}
+
+function add () {
+
+}
+
+function remove () {
+
+}
+
+function change () {
+	
+}
